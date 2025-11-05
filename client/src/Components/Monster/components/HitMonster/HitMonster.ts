316d5ee82c986047ef8ea90174d62a38cf578691
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2025-11-05 20:25:00 +0600

    Изменил регистр

--- client/src/Components/Monster/components/HitMonster/HitMonster.ts
+++ client/src/Components/Monster/components/HitMonster/HitMonster.ts
@@ -1,12 +1,12 @@
-import type { HitMonsterType } from "./MonsterProps";
+import type { HitMonsterType } from "../../MonsterProps";
 
 const hitMonster = ({setMonsterHealth, setAnimation}: HitMonsterType) => {
   setMonsterHealth(prev => Math.max(prev - (Math.floor(Math.random() * 10) + 1), 0));
 
   if (setAnimation) {
-  setAnimation("hit");
-  setTimeout(() => setAnimation(""), 300);
-}
+    setAnimation("hit");
+    setTimeout(() => setAnimation(""), 300);
+  }
 };
 
 export default hitMonster;
