aa8bb3d747f4fc81a0b0364a9514a13236fdfe37
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2025-11-05 19:48:41 +0600

    Изменил регистр

--- client/src/Components/Monster/hitMonster.ts
+++ client/src/Components/Monster/hitMonster.ts
@@ -1,6 +1,6 @@
-import type { hitMonsterType } from "./MonsterProps";
+import type { HitMonsterType } from "./MonsterProps";
 
-const hitMonster = ({setMonsterHealth, setAnimation}: hitMonsterType) => {
+const hitMonster = ({setMonsterHealth, setAnimation}: HitMonsterType) => {
   setMonsterHealth(prev => Math.max(prev - (Math.floor(Math.random() * 10) + 1), 0));
 
   if (setAnimation) {
