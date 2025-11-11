b8df9faeecda28b35e4f23a1f67d20f043d98be9
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2025-11-11 19:17:54 +0600

    Сделал показ монет

--- client/src/Components/Coins/Coins.tsx
+++ client/src/Components/Coins/Coins.tsx
@@ -1,12 +1,12 @@
 import "./coins.scss";
 
-const Coins = () => {
+const Coins = ({coins}: {coins: number}) => {
   return (
     <div className="coins">
       <span className="coins-icon"></span>
-      <span className="coins-number">0</span>
+      <span className="coins-number">{!coins ? 0 : coins}</span>
     </div>
-  )
-} 
+  );
+};
 
 export default Coins;
