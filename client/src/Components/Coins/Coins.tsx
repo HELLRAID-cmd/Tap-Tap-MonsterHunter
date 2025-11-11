ed31c043361abe3037bbdd4c8414074d4fb738e7
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2025-11-11 17:55:11 +0600

    Добавил монеты

---
+++ client/src/Components/Coins/Coins.tsx
@@ -0,0 +1,10 @@
+const Coins = () => {
+  return (
+    <div className="coins">
+      <span className="coins-icon"></span>
+      <span className="coins-number">0</span>
+    </div>
+  )
+} 
+
+export default Coins;
