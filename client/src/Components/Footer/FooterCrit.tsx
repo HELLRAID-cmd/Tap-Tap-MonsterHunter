08c96dd1d5ea37ddcc58113067b03bb2fad4e0d5
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2025-11-17 16:56:58 +0600

    исправил улучшение крита

--- client/src/Components/Footer/FooterCrit.tsx
+++ client/src/Components/Footer/FooterCrit.tsx
@@ -1,12 +1,13 @@
 import { useGame } from "../context/Context";
 
 const FooterCrit = () => {
-  const [setAttackCrit] = useGame();
+  const {setAttackCrit} = useGame();
 
   const upgradeCrit = () => {
     setAttackCrit(prev => {
-      const upgrade = prev * 2;
-      return upgrade
+      const upgrade = prev + 0.05;
+      console.log("Улучшено", upgrade.toFixed(2))
+      return upgrade > 1 ? 1 : upgrade;
     })
   }
 
