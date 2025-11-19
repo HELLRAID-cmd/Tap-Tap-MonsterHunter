b4833f963b9cb0ffee431c13598be58adffbf273
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-11-19 13:41:14 +0500

    Удалил header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,6 @@
 // import { useState } from "react";
 import { NEW_HEALTH } from "./Components/Config/Config";
-<<<<<<< HEAD
-import Header from "./Components/Header/Header";
-import { Monster } from "./Components/Monster/Monster";
-=======
 import { Monster } from "./Components/Monster/Monster"
->>>>>>> header
 import "./Styles/style.scss";
 import "./Styles/_reset.scss";
 import "./Styles/_container.scss";
@@ -14,21 +9,10 @@ import { GameProvider } from "./Components/context/Context";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <GameProvider>
-      <Header />
-      <main>
-        <Monster health={NEW_HEALTH} />
-      </main>
-      <Footer />
-    </GameProvider>
-  );
-=======
     <>
       <Monster health={NEW_HEALTH} />
     </>
   )
->>>>>>> header
 }
 
 export default App;
