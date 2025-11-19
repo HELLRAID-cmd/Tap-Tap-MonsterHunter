1b2247f139c7812877c02756fa6c870f4964fb90
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-11-19 14:53:02 +0500

    Изменил увеличение урона

--- client/src/Components/Footer/FooterAttack.tsx
+++ client/src/Components/Footer/FooterAttack.tsx
@@ -1,17 +1,22 @@
 import { useGame } from "../context/Context";
 
 const FooterAttack = () => {
-  const {setAttack} = useGame();
+  const {setAttack, attack} = useGame();
+
+  const nextUpgrade = +(attack * 0.03).toFixed(2);
   
   const upgradeAttack = () => {
     setAttack(prev => {
-      const updated = prev + 2;
+      const updated = +(prev + prev * 0.03).toFixed(2);
       return updated;
     });
   }
 
   return (
-    <button className="footer__skill-btn attack" onClick={upgradeAttack}>Атака</button>
+    <button className="footer__skill-btn attack" onClick={upgradeAttack}>
+      Атака
+      <span>+{nextUpgrade}</span>
+    </button>
   )
 }
 
