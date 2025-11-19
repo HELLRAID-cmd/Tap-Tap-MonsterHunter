ce64d8ff7c74a0525103eb19601a2c1a0fdc44c8
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-11-19 15:34:41 +0500

    Добавил цену, уровень

--- client/src/Components/Footer/FooterAttack.tsx
+++ client/src/Components/Footer/FooterAttack.tsx
@@ -1,21 +1,25 @@
+import { useState } from "react";
 import { useGame } from "../context/Context";
 
 const FooterAttack = () => {
   const {setAttack, attack} = useGame();
+  const [level, setLevel] = useState(1);
+  const [price, setPrice] = useState(1);
 
   const nextUpgrade = +(attack * 0.03).toFixed(2);
   
   const upgradeAttack = () => {
-    setAttack(prev => {
-      const updated = +(prev + prev * 0.03).toFixed(2);
-      return updated;
-    });
+    setAttack(prev => +(prev + prev * 0.03).toFixed(2));
+    setLevel(prev => prev + 1);
+    setPrice(prev => +(prev * 1.20).toFixed(2));
   }
 
   return (
     <button className="footer__skill-btn attack" onClick={upgradeAttack}>
       Атака
       <span>+{nextUpgrade}</span>
+      <span>Цена: {price}</span>
+      <span>Уровень: {level}</span>
     </button>
   )
 }
