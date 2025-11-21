92af057e184b71bd81a20e7dcbb8c70b107a493c
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-11-21 14:24:58 +0500

    Сделал пагинацию и общий дамаг

--- client/src/Components/Monster/components/HitMonster/useHitMonster.ts
+++ client/src/Components/Monster/components/HitMonster/useHitMonster.ts
@@ -1,38 +1,42 @@
+import { useState } from "react";
 import { CRIT_MULTIPLIER } from "../../../Config/Config";
 import { useGame } from "../../../context/Context";
 import type { HitMonsterType } from "../../MonsterProps";
 
 export const useHitMonster = () => {
-  const {attack, addCoins, attackCrit} = useGame();
+  const { attack, addCoins, attackCrit, setTotalDamage } = useGame();
 
   const hitMonster = ({
-    setMonsterHealth, 
-    setAnimation, 
-    setAnimationDamage, 
-    setLastDamage, 
-    monsterHealth 
+    setMonsterHealth,
+    setAnimation,
+    setAnimationDamage,
+    setLastDamage,
+    monsterHealth,
   }: HitMonsterType) => {
     // Проверка крита
-  const isCrit = Math.random() < attackCrit;
-  const critDamage = isCrit ? Math.round(attack * CRIT_MULTIPLIER) : attack;
-  
-  const newHealth = monsterHealth - critDamage;
-  setMonsterHealth(newHealth <= 0 ? 0 : newHealth);
+    const isCrit = Math.random() < attackCrit;
+    const critDamage = isCrit ? Math.round(attack * CRIT_MULTIPLIER) : attack;
 
-  // Проверка на то что монстр погиб
-  if(newHealth <= 0) {
-    setMonsterHealth(0);
-    addCoins();
-    return;
-  }
+    const newHealth = monsterHealth - critDamage;
+    setMonsterHealth(newHealth <= 0 ? 0 : newHealth);
 
-  setLastDamage(prev => [...prev, critDamage]);
-  // Через 1с убирать дамаг
-  setTimeout(() => setLastDamage(prev => prev.slice(1)), 1000);
+    // Общий дамаг
+    setTotalDamage(prev => +(prev + attack).toFixed(0));
 
-  setAnimation(isCrit ? "crit-hit" : "hit");
-  setAnimationDamage(isCrit ? "crit-damage" : "damage");
-  }
+    // Проверка на то что монстр погиб, добовляет монеты
+    if (newHealth <= 0) {
+      setMonsterHealth(0);
+      addCoins();
+      return;
+    }
 
-  return {hitMonster};
-}
+    setLastDamage((prev) => [...prev, critDamage]);
+    // Через 1с убирать дамаг
+    setTimeout(() => setLastDamage((prev) => prev.slice(1)), 1000);
+
+    setAnimation(isCrit ? "crit-hit" : "hit");
+    setAnimationDamage(isCrit ? "crit-damage" : "damage");
+  };
+
+  return { hitMonster };
+};
