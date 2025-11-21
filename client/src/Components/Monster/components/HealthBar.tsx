ce7c3abf1c2e774a93f4d31567e23c61e62199d6
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-11-21 11:29:27 +0500

    Сделал 100%

--- client/src/Components/Monster/components/HealthBar.tsx
+++ client/src/Components/Monster/components/HealthBar.tsx
@@ -3,8 +3,8 @@ import type { HealthBar } from "../MonsterProps";
 const HealthBar = ({monsterHealth, maxHealth}: HealthBar) => {
   return (
     <div className="health-bar">
-      <div className="health-bar__fill" style={{width: `${(monsterHealth / maxHealth) * 300}px`}}></div>
-      <span className="health-bar__number">{monsterHealth}хп</span>
+      <div className="health-bar__fill" style={{width: `${(monsterHealth / maxHealth) * 100}%`}}></div>
+      <span className="health-bar__number">{monsterHealth.toFixed(2)}хп</span>
     </div>
   )
 }
