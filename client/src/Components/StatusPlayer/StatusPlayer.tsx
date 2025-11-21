ab3b4d4345a3820c3fa505e5d1ad24ccbe67fe1e
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-11-21 14:25:21 +0500

    Разметка статуса

---
+++ client/src/Components/StatusPlayer/StatusPlayer.tsx
@@ -0,0 +1,35 @@
+import { useGame } from "../context/Context";
+import "./statusPlayer.scss";
+
+const StatusPlayer = () => {
+  const {attack, attackCrit, totalDamage, totalCoins, totalCoinsSpent} = useGame();
+
+  return (
+    <div className="status">
+      <ul className="status__list">
+        <li className="status__item">
+          <p className="status__item-name">Атака: </p>
+          <span className="status__item-info">{attack}</span>
+        </li>
+        <li className="status__item">
+          <p className="status__item-name">Крит: </p>
+          <span className="status__item-info">{(attackCrit * 100).toFixed(0)}%</span>
+        </li>
+        <li className="status__item">
+          <p className="status__item-name">Нанесено урона: </p>
+          <span className="status__item-info">{totalDamage}</span>
+        </li>
+        <li className="status__item">
+          <p className="status__item-name">Накоплено монет: </p>
+          <span className="status__item-info">{totalCoins}</span>
+        </li>
+        <li className="status__item">
+          <p className="status__item-name">Потрачено монет: </p>
+          <span className="status__item-info">{(totalCoinsSpent).toFixed(0)}</span>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default StatusPlayer;
