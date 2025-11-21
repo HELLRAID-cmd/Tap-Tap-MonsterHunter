882dd159046bdf323b5b9ab5bcc75576eb0982ef
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-11-21 11:26:02 +0500

    перенес функции, добавил кнопку монет

--- client/src/Components/Admin/BtnAdmin.tsx
+++ client/src/Components/Admin/BtnAdmin.tsx
@@ -1,9 +1,24 @@
-import handleKill from "./Admin";
+import { useGame } from "../context/Context";
 import type { AdminBtn } from "./AdminProps";
 
 const BtnAdmin = ({setMonsterHealth}: AdminBtn) => {
+  const {setCoins} = useGame();
+  
+  // Добавить монеты
+  const handleMoney = () => {
+    setCoins(prev => prev + 50000)
+  }
+
+  // оставить 1хп
+  const handleKill = () => {
+    setMonsterHealth(1);
+  };
+
   return (
-    <button className='btn' onClick={() => handleKill({setMonsterHealth})}>1хп</button>
+    <>
+    <button className='btn--admin-kill' onClick={handleKill}>1хп</button>
+    <button className='btn--admin-money' onClick={handleMoney}>Монеты</button>
+    </>
   )
 }
 
