3a78647194cf00cc1932523ad5a80a7bc578d497
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-11-23 13:15:29 +0500

    Вынес кнопку

---
+++ client/src/Components/Footer/FooterCritDamage.tsx
@@ -0,0 +1,10 @@
+const FooterCritDamage = () => {
+  return (
+    <button className="footer__skill-btn crit-damage">
+      <span>Урон крита</span>
+      
+    </button>
+  )
+}
+
+export default FooterCritDamage;
