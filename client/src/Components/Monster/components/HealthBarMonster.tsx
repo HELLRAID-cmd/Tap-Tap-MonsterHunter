ced25b28587ac074e090df4cd5b35d26412ac8e3
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-11-23 13:15:56 +0500

    Изменил название

---
+++ client/src/Components/Monster/components/HealthBarMonster.tsx
@@ -0,0 +1,12 @@
+import type { HealthBar } from "../MonsterProps";
+
+const HealthBarMonster = ({monsterHealth, maxHealth}: HealthBar) => {
+  return (
+    <div className="health-bar">
+      <div className="health-bar__fill" style={{width: `${(monsterHealth / maxHealth) * 100}%`}}></div>
+      <span className="health-bar__number">{monsterHealth.toFixed(2)}хп</span>
+    </div>
+  )
+}
+
+export default HealthBarMonster;
