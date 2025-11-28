8ed8b5f0689e4156700953b03421f087b4ad8231
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2025-11-28 10:53:39 +0600

    Добавил таймер в разметку

--- client/src/Components/Monster/Monster.tsx
+++ client/src/Components/Monster/Monster.tsx
@@ -9,7 +9,7 @@ import { useGame } from "../context/Context";
 
 export const Monster = ({ health }: MonsterProps) => {
 
-  const {levelMonster} = useGame();
+  const {levelMonster, timerValue} = useGame();
   
   // Рандомный цвет
   const { getRandomColor } = useMonsterActions();
@@ -31,8 +31,20 @@ export const Monster = ({ health }: MonsterProps) => {
   // Анимация цифр урона
   const [animationDamage, setAnimationDamage] = useState<string>("");
 
+  const formatTime = (seconds: number) => {
+    const h = Math.floor(seconds / 3600);
+    const m = Math.floor((seconds % 3600) / 60);
+    const s = seconds % 60;
+
+    return `${String(h).padStart(2, "0")}:${String(m).padStart(
+      2,
+      "0"
+    )}:${String(s).padStart(2, "0")}`;
+  };
+
   return (
     <div className="monster">
+      <p>timer: {formatTime(timerValue)}</p>
       <p className="monster-lvl">Уровень: {levelMonster}</p>
       <div className="monster-wrapper">
         {/* Цифры урона */}
