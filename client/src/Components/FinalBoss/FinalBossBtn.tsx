69926688e014bb16f8d59ce02815749915880f21
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2025-12-04 20:10:32 +0600

    Добавил логику кнопки

--- client/src/Components/FinalBoss/FinalBossBtn.tsx
+++ client/src/Components/FinalBoss/FinalBossBtn.tsx
@@ -1,8 +1,11 @@
+import { useGame } from "../context/Context";
 import "./FinalBoss.scss";
 
 const FinalBossBtn = () => {
+  const {setIsFinalBoss} = useGame();
+
   return (
-    <button className="final-boss__btn">Сразиться с финальным боссом</button>
+    <button className="final-boss__btn" onClick={() => setIsFinalBoss(true)}>Сразиться с финальным боссом</button>
   )
 }
 
