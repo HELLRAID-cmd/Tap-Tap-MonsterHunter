f59e2489a95a9d0110905b646ce2ab67bdf89bd9
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2025-12-08 22:02:35 +0600

    Импортировал finalBoss

--- client/src/Components/context/Providers.tsx
+++ client/src/Components/context/Providers.tsx
@@ -1,10 +1,11 @@
+import { FinalBossProvider } from "../FinalBoss/FinalBossContext";
 import { AttackProvider } from "./AttackContext";
 import { CoinsProvider } from "./CoinsContext";
 import { GameProvider } from "./Context";
 import { CritProvider } from "./CritContext";
 import { CritDamageProvider } from "./CritDamageContext";
 
-const providersArr = [GameProvider, AttackProvider, CritProvider,CritDamageProvider, CoinsProvider];
+const providersArr = [GameProvider, AttackProvider, CritProvider,CritDamageProvider, CoinsProvider, FinalBossProvider];
 
 export const Provider = ({ children }: { children: React.ReactNode }) => {
   return providersArr.reduce(
