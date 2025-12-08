3c0e43421a49f604b99703d24be9692c308af436
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2025-12-08 18:55:24 +0600

    Шкала заполнение регена хп

---
+++ client/src/Components/FinalBoss/FinalBossRegen.tsx
@@ -0,0 +1,9 @@
+const FinalBossRegen = () => {
+  return (
+    <div className="final-boss__regen">
+      <div className="final-boss__fill"></div>
+    </div>
+  )
+}
+
+export default FinalBossRegen;
