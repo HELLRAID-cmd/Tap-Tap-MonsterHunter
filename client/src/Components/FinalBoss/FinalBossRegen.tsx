5d8caa7c838049ba9f489df8256d78c0b954bc88
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2025-12-08 22:03:49 +0600

    Сделал реген хп босса

--- client/src/Components/FinalBoss/FinalBossRegen.tsx
+++ client/src/Components/FinalBoss/FinalBossRegen.tsx
@@ -1,9 +1,23 @@
+import { useEffect } from "react";
+import { useFinalBoss } from "./FinalBossContext";
+import { FINAL_BOSS_REGEN, FINAL_BOSS_TIMER } from "../Config/Config";
+
 const FinalBossRegen = () => {
+  const { setFinalBossHp } = useFinalBoss();
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setFinalBossHp((prev) => Math.min(prev + FINAL_BOSS_REGEN, 700000));
+    }, FINAL_BOSS_TIMER); 
+
+    return () => clearInterval(timer);
+  }, [setFinalBossHp]);
+
   return (
     <div className="final-boss__regen">
       <div className="final-boss__fill"></div>
     </div>
-  )
-}
+  );
+};
 
 export default FinalBossRegen;
