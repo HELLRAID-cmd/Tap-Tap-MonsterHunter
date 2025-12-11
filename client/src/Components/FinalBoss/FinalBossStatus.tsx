a8be00a6e930fbd1d64a4e4866e132ac303a038d
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2025-12-11 20:29:54 +0600

    Изменил название классов

--- client/src/Components/FinalBoss/FinalBossStatus.tsx
+++ client/src/Components/FinalBoss/FinalBossStatus.tsx
@@ -12,43 +12,43 @@ export const FinalBossStatus = () => {
   const {totalDamage, totalCoins, totalCoinsSpent, statusClick} = useGame();
   
   return (
-    <div className="status">
-      <ul className="status__list">
-        <li className="status__item">
-          <p className="status__item-name">Атака: </p>
-          <span className="status__item-info">{attack}</span>
+    <div className="final-boss__status">
+      <ul className="final-boss__list">
+        <li className="final-boss__item">
+          <p className="final-boss__item-name">Атака: </p>
+          <span className="final-boss__item-info">{attack}</span>
         </li>
-        <li className="status__item">
-          <p className="status__item-name">Шанс крит: </p>
-          <span className="status__item-info">
+        <li className="final-boss__item">
+          <p className="final-boss__item-name">Шанс крит: </p>
+          <span className="final-boss__item-info">
             {(attackCrit * 100).toFixed(0)}%
           </span>
         </li>
-        <li className="status__item">
-          <p className="status__item-name">Множитель крита: </p>
-          <span className="status__item-info">{critDamage.toFixed(1)}x</span>
+        <li className="final-boss__item">
+          <p className="final-boss__item-name">Множитель крита: </p>
+          <span className="final-boss__item-info">{critDamage.toFixed(1)}x</span>
         </li>
-        <li className="status__item">
-          <p className="status__item-name">Множитель монет: </p>
-          <span className="status__item-info">
+        <li className="final-boss__item">
+          <p className="final-boss__item-name">Множитель монет: </p>
+          <span className="final-boss__item-info">
             {coinsMultiplier.toFixed(1)}x
           </span>
         </li>
-        <li className="status__item">
-          <p className="status__item-name">Нанесено урона: </p>
-          <span className="status__item-info">{totalDamage}</span>
+        <li className="final-boss__item">
+          <p className="final-boss__item-name">Нанесено урона: </p>
+          <span className="final-boss__item-info">{totalDamage}</span>
         </li>
-        <li className="status__item">
-          <p className="status__item-name">Накоплено монет: </p>
-          <span className="status__item-info">{totalCoins}</span>
+        <li className="final-boss__item">
+          <p className="final-boss__item-name">Накоплено монет: </p>
+          <span className="final-boss__item-info">{totalCoins}</span>
         </li>
-        <li className="status__item">
-          <p className="status__item-name">Потрачено монет: </p>
-          <span className="status__item-info">{(totalCoinsSpent).toFixed(0)}</span>
+        <li className="final-boss__item">
+          <p className="final-boss__item-name">Потрачено монет: </p>
+          <span className="final-boss__item-info">{(totalCoinsSpent).toFixed(0)}</span>
         </li>
-        <li className="status__item">
-          <p className="status__item-name">Всего ударов: </p>
-          <span className="status__item-info">{statusClick}</span>
+        <li className="final-boss__item">
+          <p className="final-boss__item-name">Всего ударов: </p>
+          <span className="final-boss__item-info">{statusClick}</span>
         </li>
       </ul>
     </div>
