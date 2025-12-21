a88625b5ae4d19dc6262bf79394066903fa6a1db
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-12-21 14:58:41 +0500

    Исправил переменные

--- client/src/Components/context/Context.tsx
+++ client/src/Components/context/Context.tsx
@@ -4,7 +4,7 @@ import {
   COINS,
   CRIT_BONUS_MONEY,
   CRIT_DAMAGE_BONUS_MONEY,
-  DEMO__INFO,
+  DEMO_INFO,
   DEMO_GAME,
   MONSTER_LEVEL,
   STATUS_CLICK,
@@ -60,10 +60,10 @@ export const GameProvider = ({ children }: { children: React.ReactNode }) => {
   const [startGame, setStartGame] = useState(false);
   const [isFinalBoss, setIsFinalBoss] = useState(false);
   const [isDemo, setIsDemo] = useState(DEMO_GAME);
-  const [isDemoInfo, setIsDemoInfo] = useState(DEMO__INFO);
+  const [isDemoInfo, setIsDemoInfo] = useState(DEMO_INFO);
 
   const [timerValue, setTimerValue] = useState(0);
-  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  const intervalRef = useRef<number | null>(null);
 
   const { coinsFooter } = useCoinsFooter();
   const { critLevelDamage } = useCritDamage();
